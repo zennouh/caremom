94a5ace53f655e687a02e0305dd9af4f4eb5b911
nouh <nouh.nowile@gmail.com>
2026-04-01 20:26:27 +0100

    create migration file for application, jobs

---
+++ database/migrations/2026_03_28_130441_create_candidates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("mom_id");
+            $table->string('resume')->nullable();
+            $table->integer('years_experience')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
---
+++ database/migrations/2026_03_28_130602_create_applications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_id')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('mom_id');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->text('cover_letter')->nullable();
+            $table->timestamp('applied_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
