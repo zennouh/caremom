b01c9cf7e69ad95731de5e2c32167c03bfb4b1e2
nouh <nouh.nowile@gmail.com>
2026-04-19 13:40:38 +0100

    remove the Candidat function in the application model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -9,17 +9,11 @@ class Application extends Model
     protected $fillable = [
         'job_id',
         'mom_id',
-        'candidate_id',
         'cover_letter',
         'status'
     ];
     public function job()
     {
         return $this->belongsTo(Job::class);
-    }
-
-    public function candidate()
-    {
-        return $this->belongsTo(Candidate::class);
     }
 }
