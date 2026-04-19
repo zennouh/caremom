09d9b53c2ab575baf4f2f26ad44c45692e54f19e
nouh <nouh.nowile@gmail.com>
2026-04-19 13:42:26 +0100

    add index function to display companies

--- app/Http/Controllers/EnterpriseJobController.php
+++ app/Http/Controllers/EnterpriseJobController.php
@@ -5,25 +5,50 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class EnterpriseJobController extends Controller
 {
-    // create job
+    public function index(Request $request)
+    {
+        if ($request->has('company_id')) {
+
+            $companyId = $request->query('company_id');
+            return Job::where('company_id', $companyId)->get();
+        } else {
+            return Job::all();
+        }
+    }
+
     public function store(Request $request)
     {
-        $data = $request->validate([
+        $validator = Validator::make($request->all(), [
             'title' => 'required|string',
             'description' => 'required|string',
+            'company_id' => 'required|integer',
             'company_name' => 'required|string',
             'location' => 'nullable|string',
             'salary' => 'nullable|numeric',
-            'employment_type' => 'required|string',
-            "experience_level" => 'required|string'
+            'employment_type' => 'required|in:full-time,part-time,remote',
+            "experience_level" => 'required|in:junior,mid,senior'
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'errors' => $validator->errors()
+                ],
+                422,
+            );
+        }
+
+        $validated = $validator->validated();
 
         /**
          {
         "title": "Software Engineer",
+        "company_id": 1,
         "description": "We are looking for a skilled software engineer to join our team.",
         "company_name": "Tech Solutions Inc.",
         "location": "San Francisco, CA",
@@ -31,29 +56,31 @@ class EnterpriseJobController extends Controller
         "employment_type": "Full-time",
         "experience_level": "Mid"
         }
+
          */
 
-        $job = Job::create($data);
+        $job = Job::create($validated);
 
         return response()->json(
             [
                 'message' => 'Job created successfully',
+                'job' => $job
             ],
             201,
         );
     }
 
-    // update job
-    public function update(Request $request, $id)
-    {
-        $job = Job::findOrFail($id);
 
-        $job->update($request->all());
+    // public function update(Request $request, $id)
+    // {
+    //     $job = Job::findOrFail($id);
 
-        return response()->json($job);
-    }
+    //     $job->update($request->all());
 
-    // delete job
+    //     return response()->json($job);
+    // }
+
+
     public function destroy($id)
     {
         Job::findOrFail($id)->delete();
@@ -63,10 +90,71 @@ class EnterpriseJobController extends Controller
         ]);
     }
 
-    // list applicants
+
     public function applicants($jobId)
     {
-        return Application::where('job_id', $jobId)
+        $connection = DB::connection("auth_db");
+        $usersTable = $connection->table("users");
+        /// empolyer 
+        $apps = Application::where('job_id', $jobId)
             ->get();
+
+        $apps->each(function ($app) use ($usersTable) {
+            $user = $usersTable
+                ->select('id', 'name', 'email', 'phone', 'avatar')
+                ->where('id', $app->mom_id)
+                ->first();
+            $app->mom = $user;
+        });
+
+        return $apps;
+        /**
+         *
+[
+    {
+        "id": 1,
+        "job_id": 2,
+        "mom_id": 3,
+        "status": "pending",
+        "cover_letter": "please i need it, please i need it, please i need it, please i need it,",
+        "applied_at": "2026-04-14 08:24:43",
+        "created_at": "2026-04-14T08:24:42.000000Z",
+        "updated_at": "2026-04-14T08:24:42.000000Z",
+        "mom": {
+            "id": 3,
+            "name": "jesika2004",
+            "email": "jesika2004@gmail.com",
+            "phone": null,
+            "avatar": null
+        }
+    }
+]
+         */
+    }
+
+    public function updateStatus(Request $request, $applicationId)
+    {
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|in:accepted,rejected'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'errors' => $validator->errors()
+                ],
+                422,
+            );
+        }
+
+        $validated = $validator->validated();
+
+        $application = Application::findOrFail($applicationId);
+
+        $application->update([
+            'status' => $validated['status']
+        ]);
+
+        return response()->json($application);
     }
 }
